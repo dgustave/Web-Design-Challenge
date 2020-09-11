ec823a9d79ac215096a204a2fe5da3d821434195
donleyg <donleyg@users.noreply.github.com>
2020-09-11 00:11:12 -0400

    base complete

--- app.py
+++ app.py
@@ -2,13 +2,27 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", endpoint='home')
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
+@app.route("/compare", endpoint='compare')
+def compare():
+    return render_template("compare.html")
+@app.route("/humidity", endpoint='humidity')
+def humidity():
+    return render_template("humidity.html")
+@app.route("/tempature", endpoint='tempature')
+def tempature():
+    return render_template("tempature.html")
+@app.route("/windspeed", endpoint='windspeed')
+def windspeed():
+    return render_template("windspeed.html")
+@app.route("/cloudiness", endpoint='cloudiness')
+def compare(cloudiness):
+    return render_template("cloudiness.html")
 
 #run the app
 if __name__ == "__main__":
